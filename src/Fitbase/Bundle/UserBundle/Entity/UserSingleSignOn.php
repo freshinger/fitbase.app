800198cfdc393cce7da88ff13a3a2d81023b1ded
Alex Woroschilow <alex@fitbase.de>
2014-12-02 16:47:46 +0100

    update emails, dashboard and other

---
+++ src/Fitbase/Bundle/UserBundle/Entity/UserSingleSignOn.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Fitbase\Bundle\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserSingleSignOn
+ */
+class UserSingleSignOn
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    /**
+     * @var boolean
+     */
+    private $processed;
+
+    /**
+     * @var \DateTime
+     */
+    private $processedDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return UserSingleSignOn
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return UserSingleSignOn
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set processed
+     *
+     * @param boolean $processed
+     * @return UserSingleSignOn
+     */
+    public function setProcessed($processed)
+    {
+        $this->processed = $processed;
+
+        return $this;
+    }
+
+    /**
+     * Get processed
+     *
+     * @return boolean
+     */
+    public function getProcessed()
+    {
+        return $this->processed;
+    }
+
+    /**
+     * Set processedDate
+     *
+     * @param \DateTime $processedDate
+     * @return UserSingleSignOn
+     */
+    public function setProcessedDate($processedDate)
+    {
+        $this->processedDate = $processedDate;
+
+        return $this;
+    }
+
+    /**
+     * Get processedDate
+     *
+     * @return \DateTime
+     */
+    public function getProcessedDate()
+    {
+        return $this->processedDate;
+    }
+
+    /**
+     * @var \Application\Sonata\UserBundle\Entity\User
+     */
+    private $user;
+
+
+    /**
+     * Set user
+     *
+     * @param \Application\Sonata\UserBundle\Entity\User $user
+     * @return UserSingleSignOn
+     */
+    public function setUser(\Application\Sonata\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Application\Sonata\UserBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
