87b3a43dadfe91c8804c75268fdb73e4fe9a5f84
Alex Woroschilow <alex@fitbase.de>
2014-12-22 17:56:53 +0100

    add actioncode functionality

---
+++ src/Fitbase/Bundle/UserBundle/Entity/UserActioncode.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace Fitbase\Bundle\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserActioncode
+ */
+class UserActioncode
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    /**
+     * Prefix
+     *
+     * @var
+     */
+    protected $prefix;
+
+    /**
+     * Count of action codes to generate
+     * @var
+     */
+    protected $count;
+
+    /**
+     * @return mixed
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    /**
+     * @param mixed $count
+     */
+    public function setCount($count)
+    {
+        $this->count = $count;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * @param mixed $prefix
+     */
+    public function setPrefix($prefix)
+    {
+        $this->prefix = $prefix;
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return UserActioncode
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return UserActioncode
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @var integer
+     */
+    private $duration;
+
+    /**
+     * @var \Fitbase\Bundle\CompanyBundle\Entity\Company
+     */
+    private $company;
+
+    /**
+     * @var \Application\Sonata\ClassificationBundle\Entity\Category
+     */
+    private $category;
+
+
+    /**
+     * Set duration
+     *
+     * @param integer $duration
+     * @return UserActioncode
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * Get duration
+     *
+     * @return integer
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set company
+     *
+     * @param \Fitbase\Bundle\CompanyBundle\Entity\Company $company
+     * @return UserActioncode
+     */
+    public function setCompany(\Fitbase\Bundle\CompanyBundle\Entity\Company $company = null)
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * Get company
+     *
+     * @return \Fitbase\Bundle\CompanyBundle\Entity\Company
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \Application\Sonata\ClassificationBundle\Entity\Category $category
+     * @return UserActioncode
+     */
+    public function setCategory(\Application\Sonata\ClassificationBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \Application\Sonata\ClassificationBundle\Entity\Category
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $categories;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->categories = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add categories
+     *
+     * @param \Application\Sonata\ClassificationBundle\Entity\Category $categories
+     * @return UserActioncode
+     */
+    public function addCategory(\Application\Sonata\ClassificationBundle\Entity\Category $categories)
+    {
+        $this->categories[] = $categories;
+
+        return $this;
+    }
+
+    /**
+     * Remove categories
+     *
+     * @param \Application\Sonata\ClassificationBundle\Entity\Category $categories
+     */
+    public function removeCategory(\Application\Sonata\ClassificationBundle\Entity\Category $categories)
+    {
+        $this->categories->removeElement($categories);
+    }
+
+    /**
+     * @param \Doctrine\Common\Collections\Collection $categories
+     */
+    public function setCategories($categories)
+    {
+        $this->categories = $categories;
+    }
+
+    /**
+     * Get categories
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * Show object as a string
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getCode();
+    }
+}
---
+++ src/Fitbase/Bundle/UserBundle/Form/UserActioncodeForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Fitbase\Bundle\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserActioncodeForm extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('code', null, array(
+            'attr' => array(
+                'class' => 'form-control'
+            )
+        ))
+            ->add('save', 'submit', array(
+                'label' => 'Zugangscode einlösen',
+                'attr' => array(
+                    'class' => 'btn btn-primary',
+                ),
+            ));;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Fitbase\Bundle\UserBundle\Entity\UserActioncode'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'useractioncode';
+    }
+}
