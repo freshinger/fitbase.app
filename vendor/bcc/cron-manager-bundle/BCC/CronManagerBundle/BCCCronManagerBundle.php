32380acf32e48b4c0c5d71a121d361f000543620
Alex Woroschilow <alex@fitbase.de>
2015-01-28 16:51:38 +0100

    remove vendors

--- vendor/bcc/cron-manager-bundle/BCC/CronManagerBundle/BCCCronManagerBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BCC\CronManagerBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BCCCronManagerBundle extends Bundle
-{
-}
--- vendor/webonaute/doctrine-fixtures-generator-bundle/Webonaute/DoctrineFixturesGeneratorBundle/DoctrineFixturesGeneratorBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Webonaute\DoctrineFixturesGeneratorBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DoctrineFixturesGeneratorBundle extends Bundle
-{
-}
