81346c27402a4e5da539065c4990978d4e20e957
Alex Woroschilow <alex@fitbase.de>
2015-02-03 12:11:46 +0100

    update task, add log

--- src/Fitbase/Bundle/ExerciseBundle/Component/Chooser/ChooserExerciseRandom.php
+++ src/Fitbase/Bundle/ExerciseBundle/Component/Chooser/ChooserExerciseRandom.php
@@ -6,6 +6,47 @@ use Fitbase\Bundle\ExerciseBundle\Entity\Exercise;
 
 class ChooserExerciseRandom implements ChooserInterface
 {
+
+    protected $stepsTypes = array(
+        array(Exercise::MOBILISATION, Exercise::KRAEFTIGUNG),
+        array(Exercise::KRAEFTIGUNG, Exercise::MOBILISATION),
+        array(Exercise::KRAEFTIGUNG, Exercise::DAEHNUNG, Exercise::MOBILISATION),
+    );
+
+
+    protected function getExerciseStepType($step)
+    {
+        return isset($this->stepsTypes[$step]) ? $this->stepsTypes[$step] : null;
+    }
+
+    /**
+     * @param $step
+     * @param $category
+     * @param array $result
+     * @return mixed|null
+     */
+    protected function getExerciseStep($step, $category, $result = array())
+    {
+        if (($exercises = $category->getExercises($this->getExerciseStepType($step)))) {
+            $exercises = $exercises->filter(function ($entity) use ($result) {
+                foreach ($result as $exercise) {
+                    if ($exercise->getId() == $entity->getId()) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+
+            if (($collection = $exercises->toArray())) {
+                if (shuffle($collection)) {
+                    return array_shift($collection);
+                }
+            }
+        }
+        return null;
+    }
+
+
     /**
      * Process user focus and get a categories list
      * @param array $categories
@@ -14,26 +55,10 @@ class ChooserExerciseRandom implements ChooserInterface
      */
     public function choose($categories = array(), array $result = array())
     {
-        $types = array(
-            array(Exercise::MOBILISATION, Exercise::KRAEFTIGUNG),
-            array(Exercise::KRAEFTIGUNG, Exercise::MOBILISATION),
-            array(Exercise::KRAEFTIGUNG, Exercise::DAEHNUNG, Exercise::MOBILISATION),
-        );
-
-
         foreach ($categories as $category) {
-            for ($i = count($result); $i < count($types); $i++) {
-                if (($type = $types[$i])) {
-                    if (($exercises = $category->getExercises($type))) {
-
-                        do {
-
-                            $exercise = $this->exercise($exercises);
-
-                        } while (in_array($exercise, $result));
-
-                        array_push($result, $exercise);
-                    }
+            for ($i = count($result); $i < count($this->stepsTypes); $i++) {
+                if (($exercise = $this->getExerciseStep($i, $category, $result))) {
+                    array_push($result, $exercise);
                 }
             }
         }
