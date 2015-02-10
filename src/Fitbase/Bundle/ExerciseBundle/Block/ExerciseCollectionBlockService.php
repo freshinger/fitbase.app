e52b7311ef7c8030c0a3948ac90c0459d905634a
Alex Woroschilow <alex@fitbase.de>
2015-02-10 16:19:47 +0100

    update exercise page

--- src/Fitbase/Bundle/ExerciseBundle/Block/ExerciseCollectionBlockService.php
+++ src/Fitbase/Bundle/ExerciseBundle/Block/ExerciseCollectionBlockService.php
@@ -15,7 +15,7 @@ use Sonata\BlockBundle\Block\BlockContextInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ExerciseRandomBlockService extends BaseBlockService
+class ExerciseCollectionBlockService extends BaseBlockService
 {
     /**
      * Define
@@ -24,8 +24,8 @@ class ExerciseRandomBlockService extends BaseBlockService
     public function setDefaultSettings(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'category' => null,
-            'template' => 'FitbaseExerciseBundle:Block:exercise.html.twig',
+            'categories' => array(),
+            'template' => 'FitbaseExerciseBundle:Block:exercises.html.twig',
         ));
     }
 
@@ -35,30 +35,15 @@ class ExerciseRandomBlockService extends BaseBlockService
      */
     public function execute(BlockContextInterface $blockContext, Response $response = null)
     {
-        $exercise = null;
-        if (($category = $blockContext->getSetting('category'))) {
-            if (($exercises = $category->getExercises())) {
-
-                if ($exercises instanceof Collection) {
-                    $exercises = $exercises->toArray();
-                }
-
-                if (count($exercises)) {
-                    foreach ($exercises as $id => $exerciseCache) {
-                        if ($exerciseCache->getWebm() or $exerciseCache->getMp4()) {
-                            unset($exercises[$id]);
-                        }
-                    }
-                }
-
-                if (shuffle($exercises)) {
-                    $exercise = array_shift($exercises);
-                }
+        $exercises = array();
+        if (($categories = $blockContext->getSetting('categories'))) {
+            if (($category = $categories->first())) {
+                $exercises = $category->getExercises();
             }
         }
 
         return $this->renderPrivateResponse($blockContext->getSetting('template'), array(
-            'exercise' => $exercise
+            'exercises' => $exercises
         ));
     }
 
@@ -67,6 +52,6 @@ class ExerciseRandomBlockService extends BaseBlockService
      */
     public function getName()
     {
-        return 'Random exercise page (Exercise)';
+        return 'Exercise list page (Exercise)';
     }
 } 
