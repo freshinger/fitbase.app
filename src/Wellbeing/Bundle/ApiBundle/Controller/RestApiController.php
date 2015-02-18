597122a6727f5798f399407b25c3315891cbaed7
Alex Woroschilow <alex@fitbase.de>
2015-02-18 14:16:16 +0100

    update form processing

--- src/Wellbeing/Bundle/ApiBundle/Controller/RestApiController.php
+++ src/Wellbeing/Bundle/ApiBundle/Controller/RestApiController.php
@@ -113,23 +113,21 @@ class RestApiController extends WsdlApiController
         $entity = new \Wellbeing\Bundle\ApiBundle\Entity\UserState();
         $entity->setDate($this->get('datetime')->getDateTime('now'));
         $form = $this->createForm(new UserState(), $entity);
-        if ($request->get($form->getName())) {
-            $form->handleRequest($request);
-            if ($form->isValid()) {
+        $form->submit($request);
+        if ($form->isValid()) {
 
-                $repositoryUser = $this->get('entity_manager')->getRepository('Application\Sonata\UserBundle\Entity\User');
+            $repositoryUser = $this->get('entity_manager')->getRepository('Application\Sonata\UserBundle\Entity\User');
 
-                $entity->setUser($repositoryUser->find(1));
-                $this->get('entity_manager')->persist($entity);
-                $this->get('entity_manager')->flush($entity);
+            $entity->setUser($repositoryUser->find(1));
+            $this->get('entity_manager')->persist($entity);
+            $this->get('entity_manager')->flush($entity);
 
 
-                return ["user_position" => ["correct" => true]];
+            return ["user_position" => ["correct" => true]];
 
-            }
-            return new JsonResponse($request, 400);
         }
+        return new JsonResponse($form->getErrors()->__toString(), 400);
 
-        return new JsonResponse("Authentication code not found", 404);
+//        return new JsonResponse("Authentication code not found", 404);
     }
 }
