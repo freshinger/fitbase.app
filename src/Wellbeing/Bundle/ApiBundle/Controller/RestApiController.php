90215c541fa19a9a86f62fe70aa32ca194cbb4bc
Alex Woroschilow <alex@fitbase.de>
2015-02-18 14:42:48 +0100

    add log

--- src/Wellbeing/Bundle/ApiBundle/Controller/RestApiController.php
+++ src/Wellbeing/Bundle/ApiBundle/Controller/RestApiController.php
@@ -116,9 +116,9 @@ class RestApiController extends WsdlApiController
             'csrf_protection' => false
         ));
 
-        $this->get('logger')->crit('[api][rest]', $request->request->get($form->getName()));
+//        $this->get('logger')->crit('[api][rest]', $request->request->get($form->getName()));
 
-        $form->submit($request->request);
+        $form->submit($request->request->get($form->getName()));
         if ($form->isValid()) {
 
             $repositoryUser = $this->get('entity_manager')->getRepository('Application\Sonata\UserBundle\Entity\User');
