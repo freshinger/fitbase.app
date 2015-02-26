bb1ce6d1431117e0d7f0c9f0f04c2770cfb0a1a3
Alex Woroschilow <alex@fitbase.de>
2015-02-26 10:52:29 +0100

    update chooser method

--- src/Fitbase/Bundle/ExerciseBundle/Component/Chooser/ChooserExerciseRandom.php
+++ src/Fitbase/Bundle/ExerciseBundle/Component/Chooser/ChooserExerciseRandom.php
@@ -44,21 +44,39 @@ class ChooserExerciseRandom implements ChooserInterface
             }
         }
 
-        foreach ($steps as $step) {
-            foreach ($categories as $category) {
-                if (($types = $this->getExerciseStepType($step))) {
 
-                    if (($exercise = $this->getExerciseStep($step, $types, $category, $result))) {
+        foreach ($categories as $category) {
+            // Try to fill all steps with an exercises from
+            // a current category, or go to next category
+            foreach ($steps as $step) {
+
+                // Get exercise types for current step
+                if (($types = $this->getExerciseStepType($step))) {
+                    // Get exercise for selected types
+                    // with respect to already choose exercises
+                    // and categories list
+                    if (($exercise = $this->getExerciseStep($category, $types, $result))) {
                         $result[$step] = $exercise;
                     }
                 }
 
-                if (count($result) == count($this->steps)) {
+                // Break up if task stack
+                // already full for current category
+                if ($this->isTaskFull($result)) {
                     break;
                 }
             }
+
+            // Break up if task stack
+            // already full for all categories
+            if ($this->isTaskFull($result)) {
+                break;
+            }
         }
 
+        // Sort exercises to get
+        // an exercises with mobilisation
+        // already an position 1
         usort($result, function ($entity1, $entity2) {
             return $entity1->getType() < $entity2->getType() ? -1 : 1;
         });
@@ -67,12 +85,11 @@ class ChooserExerciseRandom implements ChooserInterface
     }
 
     /**
-     * @param $step
      * @param $category
      * @param array $result
      * @return mixed|null
      */
-    protected function getExerciseStep($step, $types, $category, $result = array())
+    protected function getExerciseStep($category, $types, $result = array())
     {
         if (($exercises = $category->getExercises($types))) {
 
@@ -84,6 +101,7 @@ class ChooserExerciseRandom implements ChooserInterface
                 }
                 return true;
             });
+
             // Choose a new exercise random
             // from not existed exercises
             if (($collection = $exercises->toArray())) {
@@ -103,5 +121,16 @@ class ChooserExerciseRandom implements ChooserInterface
     protected function getExerciseStepType($step)
     {
         return isset($this->steps[$step]) ? $this->steps[$step] : null;
+    }
+
+    /**
+     * Check is task full
+     *
+     * @param $result
+     * @return bool
+     */
+    protected function isTaskFull($result)
+    {
+        return (count($result) == count($this->steps));
     }
 }
