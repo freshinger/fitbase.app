1a30f83430bb03d5e843ccecbd3649b21c191514
Alex Woroschilow <alex@fitbase.de>
2015-03-16 10:42:20 +0100

    disable not visible questions

--- src/Fitbase/Bundle/QuestionnaireBundle/Controller/QuestionnaireStatisticController.php
+++ src/Fitbase/Bundle/QuestionnaireBundle/Controller/QuestionnaireStatisticController.php
@@ -14,20 +14,6 @@ use Symfony\Component\HttpFoundation\Request;
 class QuestionnaireStatisticController extends Controller
 {
     /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function questionnaireCompanyAction(Request $request)
-    {
-        if (($user = $this->get('user')->current())) {
-
-            return $this->render('FitbaseQuestionnaireBundle:QuestionnaireStatistic:questionnaire_company.html.twig', array(
-                'company' => $user->getCompany(),
-            ));
-        }
-    }
-
-    /**
      *
      * @param Request $request
      * @param null $unique
@@ -44,14 +30,63 @@ class QuestionnaireStatisticController extends Controller
             if (($companyQuestionnaire = $repositoryQuestionnaire->findOneByUniqueAndCompany($unique, $company))) {
 
                 $entity = new QuestionnaireUser();
-                $entity->setUser($user);
+                $entity->setUser(
+                    (new User())->setFocus(
+                        (new UserFocus())
+                            ->setCategories(
+                                $company->getCategories()->map(function ($companyCategory) {
+                                    return (new UserFocusCategory())
+                                        ->setCategory($companyCategory->getCategory());
+                                })
+                            )
+                    )
+                );
                 $entity->setQuestionnaire($companyQuestionnaire);
-
                 $form = $this->createForm(new QuestionnaireUserForm($this->container, $entity), array());
 
                 return $this->render('FitbaseQuestionnaireBundle:QuestionnaireStatistic:questionnaire.html.twig', array(
                     'form' => $form->createView(),
                     'questionnaire' => $companyQuestionnaire,
+                ));
+            }
+        }
+    }
+
+    /**
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function questionnaireSliceAction(Request $request, $unique = null)
+    {
+        $questions = null;
+
+        if (($user = $this->get('user')->current()) and ($company = $user->getCompany())) {
+
+            $entityManager = $this->get('entity_manager');
+            $repositoryQuestionnaire = $entityManager->getRepository('Fitbase\Bundle\QuestionnaireBundle\Entity\QuestionnaireCompany');
+            if (($questionnaireCompany = $repositoryQuestionnaire->findOneByUniqueAndCompany($unique, $company))) {
+
+                $entity = new QuestionnaireUser();
+                $entity->setUser(
+                    (new User())->setFocus(
+                        (new UserFocus())
+                            ->setCategories(
+                                $company->getCategories()->map(function ($companyCategory) {
+                                    return (new UserFocusCategory())
+                                        ->setCategory($companyCategory->getCategory());
+                                })
+                            )
+                    )
+                );
+                $entity->setSlice($questionnaireCompany);
+                $entity->setQuestionnaire($questionnaireCompany->getQuestionnaire());
+
+                $form = $this->createForm(new QuestionnaireUserForm($this->container, $entity), array());
+
+                return $this->render('FitbaseQuestionnaireBundle:QuestionnaireStatistic:questionnaire_slice.html.twig', array(
+                    'form' => $form->createView(),
+                    'questionnaire' => $questionnaireCompany,
                 ));
             }
         }
@@ -72,7 +107,6 @@ class QuestionnaireStatisticController extends Controller
             $entityManager = $this->get('entity_manager');
             $repositoryQuestionnaire = $entityManager->getRepository('Fitbase\Bundle\CompanyBundle\Entity\CompanyQuestionnaire');
             if (($companyQuestionnaire = $repositoryQuestionnaire->findOneByUniqueAndCompany($unique, $company))) {
-
 
                 $entity = new QuestionnaireUser();
                 $entity->setQuestionnaire($companyQuestionnaire);
@@ -105,33 +139,17 @@ class QuestionnaireStatisticController extends Controller
 
     /**
      *
+     *
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function questionnaireSliceAction(Request $request, $unique = null)
+    public function questionnaireCompanyAction(Request $request)
     {
-        $questions = null;
+        if (($user = $this->get('user')->current())) {
 
-        if (($user = $this->get('user')->current()) and ($company = $user->getCompany())) {
-
-            $entityManager = $this->get('entity_manager');
-            $repositoryQuestionnaire = $entityManager->getRepository('Fitbase\Bundle\QuestionnaireBundle\Entity\QuestionnaireCompany');
-            if (($questionnaireCompany = $repositoryQuestionnaire->findOneByUniqueAndCompany($unique, $company))) {
-
-                $entity = new QuestionnaireUser();
-                $entity->setUser($user);
-                $entity->setSlice($questionnaireCompany);
-                $entity->setQuestionnaire($questionnaireCompany->getQuestionnaire());
-
-                $form = $this->createForm(new QuestionnaireUserForm($this->container, $entity), array());
-
-                return $this->render('FitbaseQuestionnaireBundle:QuestionnaireStatistic:questionnaire_slice.html.twig', array(
-                    'form' => $form->createView(),
-                    'questionnaire' => $questionnaireCompany,
-                ));
-            }
+            return $this->render('FitbaseQuestionnaireBundle:QuestionnaireStatistic:questionnaire_company.html.twig', array(
+                'company' => $user->getCompany(),
+            ));
         }
     }
-
-
 }
