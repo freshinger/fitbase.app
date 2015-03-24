652ffbffc96ff953fee3e0f9185637a6218f41c3
Alex Woroschilow <alex@fitbase.de>
2015-03-24 10:59:23 +0100

    update password chars

--- src/Fitbase/Bundle/UserBundle/Service/ServiceCodegenerator.php
+++ src/Fitbase/Bundle/UserBundle/Service/ServiceCodegenerator.php
@@ -21,7 +21,7 @@ class ServiceCodegenerator extends ContainerAware
     public function password($length = 10)
     {
         $code = '';
-        $string = 'abcdefghijkmnprstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789!?=*';
+        $string = 'abcdefhkmnprstuvwxyzABCDEFGHKLMNPQRSTUVWXYZ23456789!?';
         for ($i = 0; $i < $length; $i++) {
             $code .= $string{rand(0, strlen($string) - 1)};
         }
