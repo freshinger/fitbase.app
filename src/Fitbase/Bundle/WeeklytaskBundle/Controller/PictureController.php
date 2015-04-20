7bde28be5ee1e31b773ea71cb5c23de9388b114d
Alex Woroschilow <alex@fitbase.de>
2015-04-20 15:53:26 +0200

    improve weeklytask images

---
+++ src/Fitbase/Bundle/WeeklytaskBundle/Controller/PictureController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Fitbase\Bundle\WeeklytaskBundle\Controller;
+
+use Fitbase\Bundle\GamificationBundle\Entity\GamificationDialogQuestion;
+use Fitbase\Bundle\GamificationBundle\Entity\GamificationUserDialogAnswer;
+use Fitbase\Bundle\GamificationBundle\Entity\GamificationUserEmotion;
+use Fitbase\Bundle\GamificationBundle\Event\GamificationUserDialogAnswerEvent;
+use Fitbase\Bundle\GamificationBundle\Event\GamificationUserEmotionEvent;
+use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerBooleanForm;
+use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerFeedbackForm;
+use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerFinishForm;
+use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerForm;
+use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerNoticeForm;
+use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerTextForm;
+use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerTrashForm;
+use Fitbase\Bundle\GamificationBundle\Form\GamificationUserEmotionForm;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\Null;
+
+class PictureController extends Controller
+{
+    /**
+     * Display company forest
+     * @param Request $request
+     * @return Response
+     */
+    public function weeklytaskAction(Request $request, $unique)
+    {
+        $entityManager = $this->container->get('entity_manager');
+        $repositoryWeeklytaskUser = $entityManager->getRepository('Fitbase\Bundle\WeeklytaskBundle\Entity\WeeklytaskUser');
+
+
+        if (($user = $this->get('user')->current())) {
+            if (($weeklytaskUser = $repositoryWeeklytaskUser->findOneByUserAndUnique($user, $unique))) {
+                if (($weeklytask = $weeklytaskUser->getTask())) {
+
+                    if (($content = $weeklytask->getContent())) {
+                        \phpQuery::newDocumentHTML($content);
+
+                        if (($images = pq('img'))) {
+                            if (($image = (isset($images[0]) ? pq($images[0]) : null)) != null) {
+
+                                $imagick2 = new \Imagick();
+                                $imagick2->readImageBlob(\file_get_contents($image->attr('src')));
+                                $imagick2->scaleImage(360, 0);
+                                return new Response($imagick2, 200, array(
+                                    'Content-Type' => 'image/png',
+                                    'Content-Disposition' => 'inline; filename="forest.png"'
+                                ));
+                            }
+                        }
+
+                        if (($categories = $weeklytask->getCategories())) {
+                            if (($category = $categories->first())) {
+                                if (($media = $category->getMedia())) {
+
+                                    $root = $this->get('kernel')->getRootDir() . '/../web';
+                                    $path = $this->container->get('sonata.media.twig.extension')->path($media, 'preview');
+
+                                    $imagick2 = new \Imagick();
+                                    $imagick2->readImageBlob(\file_get_contents($root . $path));
+                                    $imagick2->scaleImage(360, 0);
+
+                                    return new Response($imagick2, 200, array(
+                                        'Content-Type' => 'image/png',
+                                        'Content-Disposition' => 'inline; filename="forest.png"'
+                                    ));
+                                }
+
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+        
+        $imagick = new \Imagick();
+        $imagick->setBackgroundColor(new \ImagickPixel('transparent'));
+        $imagick->readImageBlob($this->renderView('Gamification/Picture/Forest.html.twig', array()));
+        $imagick->scaleImage(360, 0);
+        $imagick->setImageFormat("png");
+
+        return new Response($imagick, 200, array(
+            'Content-Type' => 'image/png',
+            'Content-Disposition' => 'inline; filename="forest.png"'
+        ));
+
+    }
+}
