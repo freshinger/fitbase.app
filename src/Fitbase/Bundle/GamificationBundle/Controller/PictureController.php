a155f4769e44cbe716d083a8de09fb370c7e8aa2
Alex Woroschilow <alex@fitbase.de>
2015-04-29 12:06:58 +0200

    fix responseive issues

--- src/Fitbase/Bundle/GamificationBundle/Controller/PictureController.php
+++ src/Fitbase/Bundle/GamificationBundle/Controller/PictureController.php
@@ -15,9 +15,13 @@ use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerNoticeFor
 use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerTextForm;
 use Fitbase\Bundle\GamificationBundle\Form\GamificationUserDialogAnswerTrashForm;
 use Fitbase\Bundle\GamificationBundle\Form\GamificationUserEmotionForm;
+use Graph;
+use JpGraph\JpGraph;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use TextProperty;
+use UniversalTheme;
 
 class PictureController extends Controller
 {
@@ -31,7 +35,8 @@ class PictureController extends Controller
         $imagick = new \Imagick();
         $imagick->setBackgroundColor(new \ImagickPixel('transparent'));
         $imagick->readImageBlob($this->renderView('Gamification/Picture/Avatar.html.twig', array()));
-        $imagick->scaleImage(360, 0);
+        $imagick->scaleImage(800, 0);
+
 
         if (($gamification = $this->get('gamification')->current())) {
 
@@ -40,12 +45,15 @@ class PictureController extends Controller
             if (($media = $gamification->getAvatarMedia($date))) {
 
                 $root = $this->get('kernel')->getRootDir() . '/../web';
-                $path = $this->container->get('sonata.media.twig.extension')->path($media, 'icon');
+                $path = $this->container->get('sonata.media.twig.extension')->path($media, 'preview');
 
                 $imagick2 = new \Imagick();
                 $imagick2->readImageBlob(\file_get_contents($root . $path));
+                $imagick2->adaptiveResizeImage(250, 250);
+
                 $imagick->setImageVirtualPixelMethod(\Imagick::VIRTUALPIXELMETHOD_TRANSPARENT);
-                $imagick->compositeImage($imagick2, \Imagick::COMPOSITE_DEFAULT, 200, 73);
+
+                $imagick->compositeImage($imagick2, \Imagick::COMPOSITE_DEFAULT, 440, 155);
             }
         }
 
@@ -59,12 +67,14 @@ class PictureController extends Controller
                 if (($media = $gamification->getTreeMedia($date, $points))) {
 
                     $root = $this->get('kernel')->getRootDir() . '/../web';
-                    $path = $this->container->get('sonata.media.twig.extension')->path($media, 'icon');
+                    $path = $this->container->get('sonata.media.twig.extension')->path($media, 'preview');
 
                     $imagick2 = new \Imagick();
                     $imagick2->readImageBlob(\file_get_contents($root . $path));
+                    $imagick2->adaptiveResizeImage(380, 380);
+
                     $imagick->setImageVirtualPixelMethod(\Imagick::VIRTUALPIXELMETHOD_TRANSPARENT);
-                    $imagick->compositeImage($imagick2, \Imagick::COMPOSITE_DEFAULT, 50, 44);
+                    $imagick->compositeImage($imagick2, \Imagick::COMPOSITE_DEFAULT, 30, 28);
                 }
             }
         }
@@ -129,7 +139,121 @@ class PictureController extends Controller
         $imagick = new \Imagick();
         $imagick->setBackgroundColor(new \ImagickPixel('transparent'));
         $imagick->readImageBlob($this->renderView('Gamification/Picture/Forest.html.twig', array()));
-        $imagick->scaleImage(360, 0);
+        $imagick->adaptiveResizeImage(718, 718);
+
+        $imagick->setImageFormat("png");
+
+        return new Response($imagick, 200, array(
+            'Content-Type' => 'image/png',
+            'Content-Disposition' => 'inline; filename="forest.png"'
+        ));
+    }
+
+    /**
+     * Display user statistic graph
+     * @param Request $request
+     * @return Response
+     */
+    public function statisticAction(Request $request)
+    {
+        $imagick = new \Imagick();
+        $imagick->setBackgroundColor(new \ImagickPixel('transparent'));
+        $imagick->readImageBlob($this->renderView('Gamification/Picture/Avatar.html.twig', array()));
+
+        if (($user = $this->container->get('user')->current())) {
+            if (($statistics = $this->container->get('statistic')->statistic($user))) {
+
+                $datetime = $this->container->get('datetime');
+
+                if (empty($statistics)) {
+                    $statistics = array(array(
+                        'date' => $datetime->getDateTime('now'),
+                        'count_point_total' => 0,
+                    ));
+                }
+
+                $cache = array();
+                foreach ($statistics as $statistic) {
+                    if (($data = isset($statistic['date']) ? $statistic['date'] : null)) {
+
+                        $index = (int)"{$data->format('Y')}{$data->format('m')}";
+
+                        if (!isset($cache[$index])) {
+                            $cache[$index] = array(
+                                "date" => $data->setTime(0, 0, 0),
+                                "points" => 0,
+                            );
+                        }
+
+                        $cache[$index]['points'] += $statistic['count_point_total'];
+                    }
+                }
+
+
+                ksort($cache);
+
+                $summ = 0;
+                $labels = array();
+                $values = array();
+
+                $translator = $this->container->get('translator');
+                foreach ($cache as $cacheEntity) {
+                    if (($data = isset($cacheEntity['date']) ? $cacheEntity['date'] : null) !== null) {
+                        if (($summ += isset($cacheEntity['points']) ? $cacheEntity['points'] : null) !== null) {
+                            array_push($values, $summ);
+
+                            $label = $translator->trans(strtolower($data->format("F")), array(), 'FitbaseGamificationBundle');
+                            array_push($labels, $label);
+                        }
+                    }
+                }
+
+                JpGraph::load();
+                JpGraph::module('line');
+                JpGraph::module('bar');
+
+
+                $graph = new Graph(660, 654, 'auto');
+                $graph->SetScale("textlin");
+
+                $graph->SetTheme(new UniversalTheme);
+                $graph->SetMargin(70, 0, 20, 50);
+                $graph->img->SetAngle(0);
+
+                $graph->SetBox(false);
+
+                $graph->ygrid->Show(false);
+                $graph->ygrid->SetFill(false);
+
+                $graph->xaxis->SetFont(FF_VERDANA, FS_NORMAL, 30);
+                $graph->xaxis->SetTickLabels($labels);
+
+
+
+                $graph->yaxis->SetFont(FF_VERDANA, FS_NORMAL, 20);
+                $graph->yaxis->HideLine(false);
+                $graph->yaxis->HideTicks(false, false);
+
+                $graph->SetBackgroundGradient('#FFFFFF', '#FFFFFF', GRAD_HOR, BGRAD_PLOT);
+
+                $b1plot = new \BarPlot($values);
+                $b1plot->SetFillGradient("#c0e3e8", "#c0e3e8", GRAD_HOR);
+                $b1plot->SetWidth(120);
+                $b1plot->SetWeight(0);
+                $graph->Add($b1plot);
+                $graph->Stroke(_IMG_HANDLER);
+
+                //Start buffering
+                ob_start();
+                $graph->img->Stream();
+                $image = ob_get_contents();
+                ob_end_clean();
+
+                $imagick->readImageBlob($image);
+            }
+        }
+
+        $imagick->adaptiveResizeImage(718, 718);
         $imagick->setImageFormat("png");
 
         return new Response($imagick, 200, array(
--- src/Fitbase/Bundle/KnowledgeBundle/Controller/PictureController.php
+++ src/Fitbase/Bundle/KnowledgeBundle/Controller/PictureController.php
@@ -13,23 +13,24 @@ class PictureController extends Controller
         $imagick = new \Imagick();
         $imagick->setBackgroundColor(new \ImagickPixel('transparent'));
         $imagick->readImageBlob($this->renderView('Knowledge/Picture/Knowledge.html.twig', array()));
-        $imagick->scaleImage(360, 0);
+        $imagick->adaptiveResizeImage(718, 718);
+
         $imagick->setImageFormat("png");
 
         $rectangle = new \ImagickDraw();
         $rectangle->setFillColor(new \ImagickPixel('#ffffff'));
-        $rectangle->rectangle(100, 80, 320, 220);
+        $rectangle->rectangle(310, 20, 690, 680);
         $imagick->drawImage($rectangle);
 
         if (($knowledge = $this->get('knowledge')->current())) {
 
             $draw = new \ImagickDraw();
             $draw->setFont('Verdana');
-            $draw->setFontSize(12);
+            $draw->setFontSize(32);
             $draw->setFillColor(new \ImagickPixel('#000000'));
             $draw->setStrokeAntialias(true);
             $draw->setTextAntialias(true);
-            $draw->annotation(105, 98, wordwrap($knowledge->getContent(), 37));
+            $draw->annotation(330, 60, wordwrap($knowledge->getContent(), 21));
             $imagick->drawImage($draw);
         }
 
@@ -39,12 +40,13 @@ class PictureController extends Controller
             if (($media = $gamification->getAvatarMedia($date))) {
 
                 $root = $this->get('kernel')->getRootDir() . '/../web';
-                $path = $this->container->get('sonata.media.twig.extension')->path($media, 'icon');
+                $path = $this->container->get('sonata.media.twig.extension')->path($media, 'thumbnail');
 
                 $imagick2 = new \Imagick();
                 $imagick2->readImageBlob(\file_get_contents($root . $path));
+                $imagick2->adaptiveResizeImage(300, 300);
                 $imagick->setImageVirtualPixelMethod(\Imagick::VIRTUALPIXELMETHOD_TRANSPARENT);
-                $imagick->compositeImage($imagick2, \Imagick::COMPOSITE_DEFAULT, 20, 163);
+                $imagick->compositeImage($imagick2, \Imagick::COMPOSITE_DEFAULT, 20, 238);
             }
         }
 
