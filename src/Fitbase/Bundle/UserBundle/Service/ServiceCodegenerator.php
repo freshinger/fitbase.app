1ba9e8d14ef277573f3653ebea0d1bb8da3ab510
Alex Woroschilow <alex@fitbase.de>
2015-05-05 10:55:47 +0200

    change codegenerator

--- src/Fitbase/Bundle/UserBundle/Service/ServiceCodegenerator.php
+++ src/Fitbase/Bundle/UserBundle/Service/ServiceCodegenerator.php
@@ -31,7 +31,7 @@ class ServiceCodegenerator extends ContainerAware
     public function code($length = 10)
     {
         $code = '';
-        $string = 'abcdefhkmnprstuvwxyzABCDEFGHKLMNPQRSTUVWXYZ23456789!?';
+        $string = 'abcdefhkmnprstuvwxyzABCDEFGHKLMNPQRSTUVWXYZ23456789';
         for ($i = 0; $i < $length; $i++) {
             $code .= $string{rand(0, strlen($string) - 1)};
         }
