84fc7c4486d4488707a0fce1676b6cce84fc39a8
Alex Woroschilow <alex@fitbase.de>
2015-05-15 16:27:48 +0200

    company consumer prototypes

---
+++ src/Fitbase/Bundle/CompanyBundle/Consumer/CompanyDisableConsumer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sensey
+ * Date: 12/02/15
+ * Time: 14:39
+ */
+namespace Fitbase\Bundle\CompanyBundle\Consumer;
+
+use Fitbase\Bundle\QuestionnaireBundle\Entity\QuestionnaireUser;
+use Fitbase\Bundle\QuestionnaireBundle\Event\QuestionnaireUserEvent;
+use Sonata\NotificationBundle\Consumer\ConsumerEvent;
+use Sonata\NotificationBundle\Consumer\ConsumerInterface;
+
+
+class CompanyDisableConsumer implements ConsumerInterface
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * Process consumer logic,
+     * create a QuestionnaireUser Object for all users
+     *
+     * @param ConsumerEvent $event
+     */
+    public function process(ConsumerEvent $event)
+    {
+    }
+}
