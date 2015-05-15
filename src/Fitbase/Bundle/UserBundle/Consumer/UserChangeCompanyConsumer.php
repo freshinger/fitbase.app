bba053d4ca2523bc4fad0849b89763462de126bb
Alex Woroschilow <alex@fitbase.de>
2015-05-15 16:28:28 +0200

    user consumer prototypes

---
+++ src/Fitbase/Bundle/UserBundle/Consumer/UserChangeCompanyConsumer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sensey
+ * Date: 12/02/15
+ * Time: 14:39
+ */
+namespace Fitbase\Bundle\UserBundle\Consumer;
+
+use Sonata\NotificationBundle\Consumer\ConsumerEvent;
+use Sonata\NotificationBundle\Consumer\ConsumerInterface;
+
+
+class UserChangeCompanyConsumer implements ConsumerInterface
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * Process message and disable user
+     * @param ConsumerEvent $event
+     */
+    public function process(ConsumerEvent $event)
+    {
+
+    }
+}
