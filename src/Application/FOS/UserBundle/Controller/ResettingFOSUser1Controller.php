3e1b89ce6e1e8a9104d08d4b6e2fc2e225e08a2f
Alex Woroschilow <alex@fitbase.de>
2015-05-22 16:34:16 +0200

    add service user interface

---
+++ src/Application/FOS/UserBundle/Controller/ResettingFOSUser1Controller.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Application\FOS\UserBundle\Controller;
+
+
+use FOS\UserBundle\Controller\ResettingController;
+use Sonata\UserBundle\Model\UserInterface;
+
+
+/**
+ * Class ResettingFOSUser1Controller
+ *
+ * @package Sonata\UserBundle\Controller
+ *
+ * @author Hugo Briand <briand@ekino.com>
+ */
+class ResettingFOSUser1Controller extends ResettingController
+{
+    /**
+     * Generate the redirection url when the resetting is completed.
+     *
+     * @param \FOS\UserBundle\Model\UserInterface $user
+     *
+     * @return string
+     */
+    protected function getRedirectionUrl(UserInterface $user)
+    {
+        return $this->container->get('router')->generate('dashboard');
+    }
+
+}
