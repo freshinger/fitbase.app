59b92e7e7b351d8cd4e6ae129ae6fb73f0a63b78
Alex Woroschilow <alex@fitbase.de>
2015-05-26 12:39:37 +0200

    redirect to correct login page after login

--- src/Fitbase/Bundle/CompanyBundle/Subscriber/RequestSubscriber.php
+++ src/Fitbase/Bundle/CompanyBundle/Subscriber/RequestSubscriber.php
@@ -29,21 +29,13 @@ class RequestSubscriber extends ContainerAware implements EventSubscriberInterfa
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
-
-        var_dump(get_class($this->container->get('router')));
-
         if (($company = $this->container->get('company')->current())) {
             if (($site = $company->getSite())) {
                 if (!$this->siteMatch($company->getSite())) {
 
                     var_dump($site->getHost());
 
-                    $context = $this->container->get('router')->getContext();
-                    $context->setHost($site->getHost());
-                    $context->setScheme($site->getScheme());
-                    $context->setPathInfo($site->getRelativePath());
-
-                    var_dump($this->container->get('router')->generate('dashboard', [], UrlGeneratorInterface::ABSOLUTE_URL));
+                    var_dump($this->container->get('company')->getCompanyUrl($company, 'dashboard'));
 
 
 //                    throw new PageNotFoundException('Requested site not available for current user: ' .
