dca771cbfc9ca3703f18c3261e371e42d8140769
Alex Woroschilow <alex@fitbase.de>
2015-06-24 14:15:55 +0200

    add admin interface for knowledges

--- src/Fitbase/Bundle/KnowledgeBundle/Entity/Knowledge.php
+++ src/Fitbase/Bundle/KnowledgeBundle/Entity/Knowledge.php
@@ -23,7 +23,7 @@ class Knowledge
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
@@ -46,10 +46,47 @@ class Knowledge
     /**
      * Get content
      *
-     * @return string 
+     * @return string
      */
     public function getContent()
     {
         return $this->content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getContentLength()
+    {
+        return mb_strlen($this->getContent());
+    }
+
+    /**
+     * @var string
+     */
+    private $format;
+
+
+    /**
+     * Set format
+     *
+     * @param string $format
+     * @return Knowledge
+     */
+    public function setFormat($format)
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    /**
+     * Get format
+     *
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
     }
 }
