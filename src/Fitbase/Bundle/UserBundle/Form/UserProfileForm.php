609331bf996a2759c286dfc6951b7cf24fb72a03
Alex Woroschilow <alex@fitbase.de>
2015-07-20 14:53:19 +0200

    fix translations

--- src/Fitbase/Bundle/UserBundle/Form/UserProfileForm.php
+++ src/Fitbase/Bundle/UserBundle/Form/UserProfileForm.php
@@ -9,58 +9,98 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class UserProfileForm extends AbstractType
 {
+    protected $class;
+
+    /**
+     * Translator object
+     *
+     * @var
+     */
+    protected $translator;
+
+    /**
+     * Class constructor
+     *
+     * @param $translator
+     */
+    public function __construct($class, $translator = null)
+    {
+        $this->class = $class;
+        $this->translator = $translator;
+    }
+
+
+    /**
+     * Translate code if translator object exists
+     *
+     * @param $code
+     * @return mixed
+     */
+    protected function _($code)
+    {
+        if (is_object($this->translator)) {
+            return $this->translator->trans($code, [], 'FitbaseUserBundle');
+        }
+
+        return $code;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('titel', 'text', array(
                 'required' => false,
-                'label' => 'Titel (optional)',
+                'label' => $this->_('user.profile.details_title'),
                 'attr' => array(
                     'class' => 'form-control',
                 )
             ))
             ->add('gender', 'choice', array(
                 'required' => false,
-                'label' => 'Anrede',
+                'label' => $this->_('user.profile.details_salutation'),
                 'choices' => array(
-                    'f' => 'Frau',
-                    'm' => 'Herr',
+                    'f' => $this->_('user.profile.details_salutation_ms'),
+                    'm' => $this->_('user.profile.details_salutation_mr'),
                 ),
-                'empty_value' => 'Bitte wählen',
+                'empty_value' => $this->_('user.profile.details_salutation_make_a_choice'),
             ))
             ->add('firstname', 'text', array(
                 'required' => false,
-                'label' => 'Vorname',
+                'label' => $this->_('user.profile.details_first_name'),
                 'attr' => array(
                     'class' => 'form-control',
                 ),
             ))
             ->add('lastname', 'text', array(
                 'required' => false,
-                'label' => 'Nachname',
+                'label' => $this->_('user.profile.details_last_name'),
                 'attr' => array(
                     'class' => 'form-control',
                 ),
             ))
             ->add('email', 'email', array(
                 'required' => false,
-                'label' => 'E-Mail-Adresse',
+                'label' => $this->_('user.profile.details_email'),
                 'attr' => array(
                     'class' => 'form-control',
                 ),
             ))
             ->add('save', 'submit', array(
-                'label' => 'Speichern',
+                'label' => $this->_('user.profile.details_save'),
                 'attr' => array(
                     'class' => 'btn btn-primary',
                 ),
             ));
     }
 
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Application\Sonata\UserBundle\Entity\User'
+            'data_class' => $this->class
         ));
     }
 
