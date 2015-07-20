04cfeb486ef06011ef3197267d5d91ecdda7a11b
Alex Woroschilow <alex@fitbase.de>
2015-07-20 14:04:57 +0200

    fix translations

--- src/Fitbase/Bundle/UserBundle/Block/AccountRemoveBlock.php
+++ src/Fitbase/Bundle/UserBundle/Block/AccountRemoveBlock.php
@@ -8,30 +8,14 @@
 namespace Fitbase\Bundle\UserBundle\Block;
 
 use Fitbase\Bundle\FitbaseBundle\Library\Block\BaseFitbaseBlock;
-use Fitbase\Bundle\ReminderBundle\Entity\ReminderUser;
-use Fitbase\Bundle\ReminderBundle\Entity\ReminderUserItem;
-use Fitbase\Bundle\ReminderBundle\Event\ReminderUserEvent;
-use Fitbase\Bundle\ReminderBundle\Event\ReminderUserItemEvent;
 use Fitbase\Bundle\ReminderBundle\Form\ReminderUserForm;
-use Fitbase\Bundle\ReminderBundle\Form\ReminderUserItemForm;
-use Fitbase\Bundle\ReminderBundle\Form\ReminderUserPauseForm;
 use Fitbase\Bundle\UserBundle\Event\UserEvent;
-use Fitbase\Bundle\UserBundle\Event\UserFocusEvent;
-use Fitbase\Bundle\UserBundle\Form\UserFocusPriorityForm;
 use Fitbase\Bundle\UserBundle\Form\UserRecoverForm;
 use Fitbase\Bundle\UserBundle\Form\UserRemoveForm;
 use Fitbase\Bundle\UserBundle\Model\UserRemove;
-use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Validator\ErrorElement;
-use Sonata\BlockBundle\Block\BaseBlockService;
-use Sonata\BlockBundle\Block\BlockContextInterface;
-use Sonata\BlockBundle\Model\BlockInterface;
 use Sonata\UserBundle\Model\UserInterface;
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
