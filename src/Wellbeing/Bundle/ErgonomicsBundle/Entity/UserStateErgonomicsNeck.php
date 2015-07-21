ac6e733381819489a4eb8126a37ca1bbef8af4f8
Alex Woroschilow <alex@fitbase.de>
2015-07-21 13:27:50 +0200

    update ergonomic position calculation

---
+++ src/Wellbeing/Bundle/ErgonomicsBundle/Entity/UserStateErgonomicsNeck.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Wellbeing\Bundle\ErgonomicsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserStateErgonomicsNeck
+ */
+class UserStateErgonomicsNeck
+{
+    /**
+     * @var float
+     */
+    private $x;
+
+    /**
+     * @var float
+     */
+    private $y;
+
+    /**
+     * @var float
+     */
+    private $z;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \Wellbeing\Bundle\ErgonomicsBundle\Entity\UserStateErgonomics
+     */
+    private $userState;
+
+    /**
+     * Class constructor
+     *
+     * @param $x
+     * @param $y
+     * @param $z
+     */
+    public function __construct($x, $y, $z)
+    {
+        $this->setX($x);
+        $this->setY($y);
+        $this->setZ($z);
+    }
+
+    /**
+     * Set x
+     *
+     * @param float $x
+     * @return UserStateErgonomicsNeck
+     */
+    public function setX($x)
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+
+    /**
+     * Get x
+     *
+     * @return float 
+     */
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * Set y
+     *
+     * @param float $y
+     * @return UserStateErgonomicsNeck
+     */
+    public function setY($y)
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+
+    /**
+     * Get y
+     *
+     * @return float 
+     */
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    /**
+     * Set z
+     *
+     * @param float $z
+     * @return UserStateErgonomicsNeck
+     */
+    public function setZ($z)
+    {
+        $this->z = $z;
+
+        return $this;
+    }
+
+    /**
+     * Get z
+     *
+     * @return float 
+     */
+    public function getZ()
+    {
+        return $this->z;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userState
+     *
+     * @param \Wellbeing\Bundle\ErgonomicsBundle\Entity\UserStateErgonomics $userState
+     * @return UserStateErgonomicsNeck
+     */
+    public function setUserState(\Wellbeing\Bundle\ErgonomicsBundle\Entity\UserStateErgonomics $userState = null)
+    {
+        $this->userState = $userState;
+
+        return $this;
+    }
+
+    /**
+     * Get userState
+     *
+     * @return \Wellbeing\Bundle\ErgonomicsBundle\Entity\UserStateErgonomics 
+     */
+    public function getUserState()
+    {
+        return $this->userState;
+    }
+}
---
+++ src/Wellbeing/Bundle/ErgonomicsBundle/Entity/UserStateErgonomicsSpineShoulder.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Wellbeing\Bundle\ErgonomicsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserStateErgonomicsSpineShoulder
+ */
+class UserStateErgonomicsSpineShoulder
+{
+    /**
+     * @var float
+     */
+    private $x;
+
+    /**
+     * @var float
+     */
+    private $y;
+
+    /**
+     * @var float
+     */
+    private $z;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \Wellbeing\Bundle\ErgonomicsBundle\Entity\UserStateErgonomics
+     */
+    private $userState;
+
+    /**
+     * Class constructor
+     *
+     * @param $x
+     * @param $y
+     * @param $z
+     */
+    public function __construct($x, $y, $z)
+    {
+        $this->setX($x);
+        $this->setY($y);
+        $this->setZ($z);
+    }
+
+    /**
+     * Set x
+     *
+     * @param float $x
+     * @return UserStateErgonomicsSpineShoulder
+     */
+    public function setX($x)
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+
+    /**
+     * Get x
+     *
+     * @return float 
+     */
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * Set y
+     *
+     * @param float $y
+     * @return UserStateErgonomicsSpineShoulder
+     */
+    public function setY($y)
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+
+    /**
+     * Get y
+     *
+     * @return float 
+     */
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    /**
+     * Set z
+     *
+     * @param float $z
+     * @return UserStateErgonomicsSpineShoulder
+     */
+    public function setZ($z)
+    {
+        $this->z = $z;
+
+        return $this;
+    }
+
+    /**
+     * Get z
+     *
+     * @return float 
+     */
+    public function getZ()
+    {
+        return $this->z;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userState
+     *
+     * @param \Wellbeing\Bundle\ErgonomicsBundle\Entity\UserStateErgonomics $userState
+     * @return UserStateErgonomicsSpineShoulder
+     */
+    public function setUserState(\Wellbeing\Bundle\ErgonomicsBundle\Entity\UserStateErgonomics $userState = null)
+    {
+        $this->userState = $userState;
+
+        return $this;
+    }
+
+    /**
+     * Get userState
+     *
+     * @return \Wellbeing\Bundle\ErgonomicsBundle\Entity\UserStateErgonomics 
+     */
+    public function getUserState()
+    {
+        return $this->userState;
+    }
+}
